fafe7ab94f31ab9dfd6334bae7f7e06b522364d3
Andrew Zaicev <mikowftv30@mail.ru>
2020-10-05 17:15:22 +0300

    Hometask 2

--- src/Semester/Main.fs
+++ src/Semester/Main.fs
@@ -9,13 +9,7 @@ module Main =
         | Thirdtask 
         | Fourthtask
         | Fifthtask
-        | Sixthtask
-        | Task7
-        | Task8
-        | Task9
-        | Task10
-        | Task11
-        | Task12
+        | Sixthtask       
         interface IArgParserTemplate with
             member s.Usage =
                 match s with
@@ -24,14 +18,7 @@ module Main =
                 | Thirdtask _ -> "Calculates indexes of array elements no larger than the specified number"
                 | Fourthtask _ -> "Calculates indexes of array elements that lie outside the range specified by numbers"
                 | Fifthtask _ -> "Changing the array elements in some places"
-                | Sixthtask _ -> "Changes element i to element j"
-                | Task7 _ -> "The Fibonacci number by recursive method"
-                | Task8 _ -> "The Fibonacci number by iterative method"
-                | Task9 _ -> "Tail recursion"
-                | Task10 _ -> "Matrix multiplication"
-                | Task11 _ -> "Matrix multiplication for log "
-                | Task12 _ -> "Fibonacci numbers up to the specified value"
-
+                | Sixthtask _ -> "Changes element i to element j"                
     [<EntryPoint>]
      let main (argv: string array) =
         try
@@ -69,59 +56,24 @@ module Main =
              printfn ("Индексы элементов вне диапазона :")
              printfn ("%A") thirdarray
          elif results.Contains Fifthtask then            
-             let dimension_array = 2          
-             let array = domashka.create_array dimension_array
+             let dimensionArray = 2          
+             let array = domashka.create_array dimensionArray
              let arrayizm = domashka.Fifthtask array
              printfn ("Измененные элементы массива")
              printfn ("%A") arrayizm
          elif results.Contains Sixthtask then
              printfn ("Введите размерность массива ")
-             let dimension_array = Console.ReadLine() |> int
-             let array:int array  = domashka.create_array dimension_array
-             let z = Console.ReadLine() |> int
-             let v = Console.ReadLine() |> int
-             if (z>=0) && (z<= dimension_array - 1) && (v>=0) && (v<=dimension_array - 1) && (z<>v) then
-                 let arrayizmeneniy = domashka.Sixthtask array z v 
-                 printfn ("Измененный массив")
-                 printfn ("%A") arrayizmeneniy
-             else printfn ("Введенные значения выходят за границы массива")
-         elif results.Contains Task7 then
-             printfn ("Введите число фибоначи, которое хотите вычислить")
-             let n = Console.ReadLine() |> int
-             let result = domashka2.Task7 n
-             printfn ("Число фибоначи равно =")
-             printfn ("%A") result            
-         elif results.Contains Task8 then
-            printfn ("Введите число фибоначи, которое хотите вычислить")
-            let t = Console.ReadLine() |> int
-            let result = domashka2.Task8 t
-            printfn ("Число фибоначи равно =")
-            printfn ("%A") result
-         elif results.Contains Task9 then
-            printfn ("Введите число фибоначи, которое хотите вычислить")
-            let q = Console.ReadLine() |> int
-            let result = domashka2.Task9 q
-            printfn ("Число фибоначи равно =")
-            printfn ("%A") result
-         elif results.Contains Task10 then
-            printfn ("Введите число фибоначи, которое хотите вычислить")
-            let q = Console.ReadLine() |> int
-            let result = domashka2.Task10 q           
-            printfn ("Число фибоначи равно =")
-            printfn ("%A") (result)
-         elif results.Contains Task11 then
-            printfn ("Введите число фибоначи, которое хотите вычислить")
-            let y = Console.ReadLine() |> int
-            let result = domashka2.Task11 y           
-            printfn ("Число фибоначи равно =")
-            printfn ("%A") (result)
-         elif results.Contains Task12 then
-            printfn ("Введите число, до которого выведутся числа фибоначи ")
-            let t = Console.ReadLine() |> int
-            let result = domashka2.Task12 t 
-            printfn ("%A") result 
-    
-
+             let dimensionArray = Console.ReadLine() |> int
+             if dimensionArray < 2 then failwith ("Array needs at least 2 elements")
+             else
+                 let array: int array  = domashka.create_array dimensionArray
+                 let z = Console.ReadLine() |> int
+                 let v = Console.ReadLine() |> int
+                 if (z >=0 ) && (z <= dimensionArray - 1) && (v >= 0) && (v <= dimensionArray - 1) && (z <> v) then
+                     let arrayizmeneniy = domashka.Sixthtask array z v 
+                     printfn ("Измененный массив")
+                     printfn ("%A") arrayizmeneniy
+                 else printfn ("Введенные значения выходят за границы массива")            
          else
             
             parser.PrintUsage() |> printfn "%s"
--- tests/Semester.Tests/Tests.fs
+++ tests/Semester.Tests/Tests.fs
@@ -5,114 +5,68 @@ open Expecto
 open Semester
 
 [<Tests>]
-let tests =
+let tests3 =
     testList "samples"
-        [ // дамаха) N1/2
+        [
             testCase "Given empty array gives empty array thirdtask" <| fun _ ->
                 let subject = domashka.Thirdtask [||] 3
-                Expect.equal subject [||] "Given empty array"
-            testCase "Given empty array gives empty array fourth task" <| fun _ ->
-                let subject = domashka.Fourthtask [||] 3 5 
-                Expect.equal subject [||] "Given empty array"
+                Expect.equal subject [||] "Given empty array"          
             testCase "Positive always greater than negative its obvious,thirdtask" <| fun _ ->
                 let subject = domashka.Thirdtask [|-2; -1; 1; 2 |] 0
                 Expect.equal subject [| 0; 1 |] "result must be equal [| 0; 1 |]"
-            testCase "Positive always greater than negative its obvious fourth task" <| fun _ ->
-                let subject = domashka.Fourthtask [|-2; -1; 1; 2 |] 0 2 
-                Expect.equal subject [| 0; 1 |] "result must be equal [| 0; 1 |]"           
-            testCase "0 to any power of 0 other than zero" <| fun _ ->
-                let subject = domashka.Ftask 0.
-                Expect.equal subject 1. "result must be equal 1."
-            testCase "the polynomial takes the value 121 for x = 3" <| fun _ ->
-                let subject = domashka.Ftask 3.
-                Expect.equal subject 121. "result must be equal 121."
+        ]
+[<Tests>]
+let tests2 =
+    testList "second task tests"
+        [
             testCase "0 to any power of 0 other than zero 2 task" <| fun _ ->
                 let subject = domashka.Stask 0.
                 Expect.equal subject 1. "result must be equal 1"
             testCase "the polynomial takes the value 121 for x = 3 2 task" <| fun _ ->
                 let subject = domashka.Stask 3.
-                Expect.equal subject 121. "result must be equal 121.0"        
-            testCase "First Fibonacci number, first task" <| fun _ ->
-                let subject = domashka2.Task7 1
-                Expect.equal subject 1 "rфывфывesult must be equal 1"
-            testCase "Second Fibonacci number, first task" <| fun _ ->
-                let subject = domashka2.Task7 2
-                Expect.equal subject 1 "result must be equal 1"
-            testCase "Third Fibonacci number, first task" <| fun _ ->
-                let subject = domashka2.Task7 3
-                Expect.equal subject 2 "result must be equal 2"
-            testCase "Twelveth Fibonacci number, first task" <| fun _ ->
-                let subject = domashka2.Task7 12
-                Expect.equal subject 144 "result must be equal 144"
-            testCase "First Fibonacci number, second task" <| fun _ ->
-                let subject = domashka2.Task8 1
-                Expect.equal subject 1 "result must be equal 1"
-            testCase "Second Fibonacci number, second task" <| fun _ ->
-                let subject = domashka2.Task8 2
-                Expect.equal subject 1 "result must be equal 1"
-            testCase "Third Fibonacci number, second task" <| fun _ ->
-                let subject = domashka2.Task8 3
-                Expect.equal subject 2 "result must be equal 2"
-            testCase "Twelveth Fibonacci number, second task" <| fun _ ->
-                let subject = domashka2.Task8 12
-                Expect.equal subject 144 "result must be equal 144"
-            testCase "First Fibonacci number, third task" <| fun _ ->
-                let subject = domashka2.Task9 1
-                Expect.equal subject 1 "result must be equal 1"
-            testCase "Second Fibonacci number, third task" <| fun _ ->
-                let subject = domashka2.Task9 2
-                Expect.equal subject 1 "result must be equal 1"
-            testCase "Third Fibonacci number, third task" <| fun _ ->
-                let subject = domashka2.Task9 3
-                Expect.equal subject 2 "result must be equal 2"
-            testCase "Twelveth Fibonacci number, third task" <| fun _ ->
-                let subject = domashka2.Task9 12
-                Expect.equal subject 144 "result must be equal "
-            testCase "First Fibonacci number, fourth task" <| fun _ ->
-                let subject = domashka2.Task10 1
-                Expect.equal subject  1 "result must be equal 1"
-            testCase "Third Fibonacci number, fourth task" <| fun _ ->
-                let subject = domashka2.Task10 3
-                Expect.equal subject 2 "result must be equal 2"
-            testCase "Twelveth Fibonacci number, fourth task" <| fun _ ->
-                let subject = domashka2.Task10 12
-                Expect.equal subject 144 "result must be equal 144"
-            testCase "First Fibonacci number, fifth task" <| fun _ ->
-                let subject = domashka2.Task11 1
-                Expect.equal subject 1 "result must be equal 1"          
-            testCase "Third Fibonacci number, fifth task" <| fun _ ->
-                let subject = domashka2.Task11 3
-                Expect.equal subject 2 "result must be equal 2"
-            testCase "Twelveth Fibonacci number, fifth task" <| fun _ ->
-                let subject = domashka2.Task11 12
-                Expect.equal subject 144 "result must be equal 144"
-            testCase "fibonnacis numbers 1" <| fun _ ->
-                let subject = domashka2.Task12 1
-                Expect.sequenceEqual subject [|1|] "result must be equal [| 1 |]"
-            testCase "fibonnacis numbers 2" <| fun _ ->
-                let subject = domashka2.Task12 2
-                Expect.sequenceEqual subject [| 1; 1 |] "result must be equal [| 1; 1 |]"
-            testCase "fibonnacis numbers 3" <| fun _ ->
-                let subject = domashka2.Task12 3
-                Expect.sequenceEqual subject [| 1; 1; 2 |] "result must be equal [| 1; 1; 2 |]"
-            testCase "Twelveth Fibonacci number, six" <| fun _ ->
-                let subject = domashka2.Task12 4
-                Expect.sequenceEqual subject [| 1; 1; 2; 3 |] "result must be equal [| 1; 1; 2; 3 |]"
-            let moduleTest n =
-                if n = 0 then 1
-                else if n < 0
-                then -n
-                else n
-            testProperty "task9 task10"
-                <| fun ( n: int ) -> Expect.equal ( domashka2.Task9 ( moduleTest n )) ( domashka2.Task10 ( moduleTest n )) "Compare task10 task9"
-            testProperty "task8 task9"
-                <| fun ( n: int ) -> Expect.equal ( domashka2.Task8  (moduleTest n )) ( domashka2.Task9 ( moduleTest n )) "Compare task8 task9"
-            testProperty "task10 task11"
-            <| fun ( n: int ) -> Expect.equal ( domashka2.Task10 ( moduleTest n )) ( domashka2.Task11 ( moduleTest n )) "Compare task10 task11"
-            ]    
-
+                Expect.equal subject 121. "result must be equal 121.0"  
+        ]
+[<Tests>]
+let tests4 =
+    testList "fourth task tests"
+        [
+            testCase "Positive always greater than negative its obvious fourth task" <| fun _ ->
+                let subject = domashka.Fourthtask [|-2; -1; 1; 2 |] 0 2 
+                Expect.equal subject [| 0; 1 |] "result must be equal [| 0; 1 |]"           
+            testCase "Given empty array gives empty array fourth task" <| fun _ ->
+                let subject = domashka.Fourthtask [||] 3 5 
+                Expect.equal subject [||] "Given empty array"
+        ]
+let tests1 =
+    testList "first task tests"
+        [
+            testCase "fifth task for x = 0" <| fun _ ->
+                let subject = domashka.Ftask 0.
+                Expect.equal subject 1. "result must be equal 1."
+            testCase "the polynomial takes the value 121 for x = 3" <| fun _ ->
+                let subject = domashka.Ftask 3.
+                Expect.equal subject 121. "result must be equal 121."
+        ]
+[<Tests>]
+let tests5 =
+    testList "fifth task tests"
+        [
+            testCase "input value" <| fun _ ->
+                let subject = domashka.Fifthtask [|1; 2|] 
+                Expect.sequenceEqual subject [|2; 1|] "result must be equal [|2; 1|]"
+            testCase "input value 2 " <| fun _ ->
+                let subject = domashka.Fifthtask [|0; -3|] 
+                Expect.sequenceEqual subject [|-3; 0|] "result must be equal [|-3; 0|]"
+        ]
+[<Tests>]
+let tests6 =
+    testList "sixth task tests"
+        [
+            testCase "input value 3" <| fun _ ->
+                let subject = domashka.Sixthtask [|0; 1; 2; 3; 4; 5|] 1 2
+                Expect.sequenceEqual subject [|0; 2; 1; 3; 4; 5|] "result must be equal [|0; 2; 1; 3; 4; 5|]"
+        ]
             
-
 
 
 
