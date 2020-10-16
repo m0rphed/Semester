7fe3223eadbdeb9a7498703ef9f9f57e9f67d8ff
Andrew Zaicev <mikowftv30@mail.ru>
2020-10-16 10:47:55 +0300

    Changed homework 3

--- src/Semester/homework3.fs
+++ src/Semester/homework3.fs
@@ -15,12 +15,12 @@ module domashka2
         elif t = 0
         then 0
         else
-            let a = [|0 .. t|]
-            a.[0] <- 1
-            a.[1] <- 1
-            for i in 2 .. t - 1 do
-                a.[i] <-  a.[i - 1] +  a.[i - 2]
-            a.[t - 1]                  
+            let a = [|0 .. t - 2|]
+            for i in 0 .. t - 2 do
+                if i = 0 || i = 1
+                then a.[i] <- i + 1
+                else a.[i] <- a.[i - 1] + a.[i - 2]
+            a.[t - 2]                  
     let Task9 q =
         if q < 0
         then failwith "Fibonacci number is not negative"
@@ -35,22 +35,16 @@ module domashka2
         if t < 0
         then failwith "Fibonacci number is not negative"
         let a = [|0 .. t|]
-        for i in 0 .. t do 
-            if i = 0
-            then a.[i] <- 0
-            elif i = 1
-            then a.[i] <- 1
-            else a.[i] <- a.[i - 1] + a.[i - 2]
+        for i in 2 .. t do 
+            a.[i] <- a.[i - 1] + a.[i - 2]
         a   
     let multiply (o: array<array<int>>) (t: array<array<int>>) = 
         let I = o.Length 
         let J = o.[0].Length
         let K = t.[0].Length
-        if I <> J
-        then failwith "Matrix needs to be square"
         let Matrix = Array.init I ( fun _ -> Array.zeroCreate J )
         for i in 0 .. I - 1 do 
-            for j in 0 .. J - 1  do 
+            for j in 0 .. J - 1 do 
                 for k in 0 .. K - 1 do 
                     Matrix.[i].[j] <- Matrix.[i].[j] + (o.[k].[j] * t.[i].[k])
         Matrix
@@ -59,18 +53,6 @@ module domashka2
         for i in 0 .. x - 1 do
             matrix.[i].[i] <- 1
         matrix
-    let Task10 n  =
-        if n < 0
-        then failwith "Fibonacci number is not negative"
-        let x = [|[|0; 1|]; [|1; 1|]|]
-        let rec help n (x: int [] []) =
-            if n = 1 
-            then x
-            elif n = 0
-            then Matrixpow0 2
-            else
-                multiply ((help (n - 1) x)) x
-        (help n x).[0].[1]
     let powMatrix (e: int [] []) x =
         if x = 0
         then Matrixpow0 e.Length
@@ -80,7 +62,12 @@ module domashka2
             let mutable k = e
             for i in 1 .. x - 1 do
                 k <- multiply k e
-            k                 
+            k             
+    let Task10 n  =
+        if n < 0
+        then failwith "Fibonacci number is not negative"
+        let x = [|[|0; 1|]; [|1; 1|]|]
+        (powMatrix x n).[0].[1]          
     let Task11 x =
         if x < 0
         then failwith "Fibonacci number is not negative"
@@ -90,10 +77,7 @@ module domashka2
             then z
             elif x = 0
             then Matrixpow0 2
-            else
-                if x % 2 = 0
-                then
-                    powMatrix (helper (x / 2)) 2
-                else
-                    multiply z (powMatrix (helper ((x-1) / 2)) 2)
+            else if x % 2 = 0
+                 then powMatrix (helper (x / 2)) 2
+                 else  multiply z (powMatrix (helper ((x-1) / 2)) 2)
         (helper x).[0].[1]
