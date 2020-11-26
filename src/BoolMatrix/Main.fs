24322f508eed10093dd645c137049754abcd3ca7
Andrew Zaicev <mikowftv30@mail.ru>
2020-11-26 18:12:57 +0300

    upd hw 6

--- src/BoolMatrix/Main.fs
+++ src/BoolMatrix/Main.fs
@@ -46,21 +46,21 @@ type CLIArguments =
                     let k = Console.ReadLine() |> int
                     let n = Console.ReadLine() |> int
                     let t = Console.ReadLine() |> int
-                    let fMatrix = generateRandomBoolMatrix (abs n) (abs k)
-                    let sMatrix = generateRandomBoolMatrix (abs k) (abs t)
-                    let first = returnMatrix (multiplyBool (createBoolMatrixFromStandart fMatrix) (createBoolMatrixFromStandart sMatrix))
+                    let fMatrix = BoolMatrix.generateRandomBoolMatrix (abs n) (abs k)
+                    let sMatrix = BoolMatrix.generateRandomBoolMatrix (abs k) (abs t)
+                    let first = BoolMatrix.returnMatrix (BoolMatrix.multiplyBool (BoolMatrix.createBoolMatrixFromStandart fMatrix) (BoolMatrix.createBoolMatrixFromStandart sMatrix))
                     printfn ("\n %A") first
-                    let second = multiply fMatrix sMatrix
+                    let second = BoolMatrix.multiply fMatrix sMatrix
                     printfn ("\n %A") second
                 elif results.Contains WriteRead
                 then
                     let y = Console.ReadLine() |> int
                     let z = Console.ReadLine() |> int
                     let x = Console.ReadLine()
-                    let u = generateRandomBoolMatrix y z
+                    let u = BoolMatrix.generateRandomBoolMatrix y z
                     printfn ("\n %A") u
-                    writeOutputMatrix u x
-                    printfn ("\n %A") (returnMatrix (readMatrix x))                    
+                    BoolMatrix.writeOutputMatrix u x
+                    printfn ("\n %A") (BoolMatrix.returnMatrix (BoolMatrix.readMatrix x))                    
                 else           
                     parser.PrintUsage() |> printfn "%s"
                 0
--- tests/BoolMatrixForTest.Tests/test.fs
+++ tests/BoolMatrixForTest.Tests/test.fs
@@ -10,18 +10,18 @@ let checkEquality =
             <| fun (k: int, n: int, t: int) ->
                 if k <> 0 && n <> 0 && t <> 0
                 then
-                    let fitstGenerate = generateRandomBoolMatrix (abs n) (abs k)
-                    let secondGenerate = generateRandomBoolMatrix (abs k) (abs t)
-                    let first = returnMatrix (multiplyBool (createBoolMatrixFromStandart fitstGenerate) (createBoolMatrixFromStandart secondGenerate))
-                    let second = multiply fitstGenerate secondGenerate
+                    let fitstGenerate = BoolMatrix.generateRandomBoolMatrix (abs n) (abs k)
+                    let secondGenerate = BoolMatrix.generateRandomBoolMatrix (abs k) (abs t)
+                    let first = BoolMatrix.returnMatrix (BoolMatrix.multiplyBool (BoolMatrix.createBoolMatrixFromStandart fitstGenerate) (BoolMatrix.createBoolMatrixFromStandart secondGenerate))
+                    let second = BoolMatrix.multiply fitstGenerate secondGenerate
                     Expect.equal first second "Needs to be equal"
             testProperty "test that write and read correctly"
             <| fun (x, y) ->
                 if x > 0 && y > 0
                 then
-                    let f = generateRandomBoolMatrix x y 
-                    writeOutputMatrix f "dasd.txt"
-                    Expect.equal f (returnMatrix (readMatrix "dasd.txt")) "needs to be equal"
+                    let f = BoolMatrix.generateRandomBoolMatrix x y 
+                    BoolMatrix.writeOutputMatrix f "dasd.txt"
+                    Expect.equal f (BoolMatrix.returnMatrix (BoolMatrix.readMatrix "dasd.txt")) "needs to be equal"
         ]
 [<Tests>]
 let checkSpecific =
@@ -29,11 +29,11 @@ let checkSpecific =
         [
            testCase "Pustoi massiv"
            <| fun _ ->
-               let first = createBoolMatrixFromStandart (generateRandomBoolMatrix 0 0)
-               let second = createBoolMatrixFromStandart (generateRandomBoolMatrix 0 0)
-               Expect.equal (multiplyBool first second) (Matrix (0, 0, [])) "Needs to be equal"
+               let first = BoolMatrix.createBoolMatrixFromStandart (BoolMatrix.generateRandomBoolMatrix 0 0)
+               let second = BoolMatrix.createBoolMatrixFromStandart (BoolMatrix.generateRandomBoolMatrix 0 0)
+               Expect.equal (BoolMatrix.multiplyBool first second) (Matrix (0, 0, [])) "Needs to be equal"
            testCase "Input values < 0"
            <| fun _ ->
-               Expect.throws (fun _ -> generateRandomBoolMatrix -1 -1 |> ignore) "Cannot create matrix" 
+               Expect.throws (fun _ -> BoolMatrix.generateRandomBoolMatrix -1 -1 |> ignore) "Cannot create matrix" 
         ]
 
