12ab41f288cd99bf37fb8f3b48d29ae807bdcc32
AndreiZaycev <71140884+AndreiZaycev@users.noreply.github.com>
2021-05-20 00:26:00 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,21 +2,19 @@
 
 set -eu
 set -o pipefail
-dotnet new tool-manifest --force
-dotnet tool install Paket
+
+# Set enviroment variales for paket and fake-cli
+PAKET_SKIP_RESTORE_TARGETS=true
+FAKE_DETAILED_ERRORS=true 
+
+dotnet tool install --global Paket
 
 echo "Adding token for calculator..."
 echo $MY_COOL_TOKEN
 dotnet paket config add-token "https://nuget.pkg.github.com/kirillgarbar/index.json" "$MY_COOL_TOKEN"
 
-echo "Adding Arithm package..."
-cd ./src/AvaloniaEditDemo
-echo "[INFO] Current path is: " ; pwd
-dotnet add package Arithm --version 1.0.2
-cd ../../
-echo "[INFO] Current path is: " ; pwd
-
 echo "Restoring dotnet tools..."
 dotnet tool restore
 
-PAKET_SKIP_RESTORE_TARGETS=true FAKE_DETAILED_ERRORS=true dotnet fake build -t "$@"
+# Start fake-cli and build repository with targets
+dotnet fake build -t "$@"
