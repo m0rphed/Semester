0b652d12d7023ee715f3824bb13293cf2d8f6f51
AndreiZaycev <71140884+AndreiZaycev@users.noreply.github.com>
2021-05-20 15:33:42 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,13 +10,11 @@ FAKE_DETAILED_ERRORS=true
 dotnet tool install --global Paket
 
 echo "Adding token for calculator..."
-echo $MY_COOL_TOKEN
+echo "Restoring dotnet tools..."
+dotnet tool restore
 paket config add-token "https://nuget.pkg.github.com/kirillgarbar/index.json" "$MY_COOL_TOKEN"
 paket config add-token 'https://www.nuget.org' "$MY_COOL_TOKEN"
 paket config add-token "https://www.myget.org/F/avalonia-ci/api/v2" "$MY_COOL_TOKEN"
-
-echo "Restoring dotnet tools..."
-dotnet tool restore
 
 # Start fake-cli and build repository with targets
 dotnet fake build -t "$@"
