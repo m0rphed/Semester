71c5aa6718a3aa3045fb80f038d87b1cb73c7422
AndreiZaycev <71140884+AndreiZaycev@users.noreply.github.com>
2021-05-20 15:29:07 +0300

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,5 +1,7 @@
 echo "Adding token for calculator..."
 paket config add-token "https://nuget.pkg.github.com/kirillgarbar/index.json" %MY_COOL_TOKEN%
+paket config add-token "https://www.nuget.org" %MY_COOL_TOKEN%
+paket config add-token "https://www.myget.org/F/avalonia-ci/api/v2" %MY_COOL_TOKEN%
 
 echo "Adding Arithm package..."
 cd src\AvaloniaEditDemo
