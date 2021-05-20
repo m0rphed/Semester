4bf7799f592902e46147f63df4cd9045aa4c6fd1
AndreiZaycev <71140884+AndreiZaycev@users.noreply.github.com>
2021-05-20 15:54:14 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,20 +1,36 @@
 #!/usr/bin/env bash
 
+# Reset
+Color_Off='\033[0m'       # Text Reset
+
+# Regular Colors
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
+
 set -eu
 set -o pipefail
 
+printf "${Red}[info] ${Cyan}Running 'dotnet tool restore'...${Color_Off}\n"
+dotnet tool restore
+
+printf "${Red}[info] ${Cyan}Adding token for calculator...${Color_Off}\n"
+printf "${Yellow}token: ${Yellow} => { $MY_COOL_TOKEN }${Color_Off}\n"
+dotnet paket config add-token "https://nuget.pkg.github.com/kirillgarbar/index.json" "$MY_COOL_TOKEN"
+# dotnet nuget add source --name "GitHub_Arithm" --username AndreiZaycev --password "$MY_COOL_TOKEN" "https://nuget.pkg.github.com/kirillgarbar/index.json" --store-password-in-clear-text
+
+printf "${Red}[info] ${Cyan}Running 'dotnet paket restore'...${Color_Off}\n"
+dotnet paket restore
+
 # Set enviroment variales for paket and fake-cli
 PAKET_SKIP_RESTORE_TARGETS=true
-FAKE_DETAILED_ERRORS=true 
+FAKE_DETAILED_ERRORS=true
 
-dotnet tool install --global Paket
-
-echo "Adding token for calculator..."
-echo "Restoring dotnet tools..."
-dotnet tool restore
-paket config add-token "https://nuget.pkg.github.com/kirillgarbar/index.json" "$MY_COOL_TOKEN"
-paket config add-token 'https://www.nuget.org' "$MY_COOL_TOKEN"
-paket config add-token "https://www.myget.org/F/avalonia-ci/api/v2" "$MY_COOL_TOKEN"
-
+printf "${Red}[info] ${Cyan}Running 'dotnet fake build -t \"\$\@\"'...${Color_Off}\n"
 # Start fake-cli and build repository with targets
 dotnet fake build -t "$@"
